61486fabf29cf92835d1f3b90c406c5e3563347b
iammagash <74823831+Magashwarahan@users.noreply.github.com>
2022-03-06 16:34:23 +0530

    Add files via upload

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div style={{textAlign: "center" , marginBottom:10}}>
+        Made with 🤍 by Magashwarahan and Sanjeev
+    </div>
+  )
+}
+
+export default Footer
